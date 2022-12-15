cff27e648184f34b955bb75c16479e9e9f18deac
Vadim V P <Everestxxx555@yandex.ru>
2022-12-15 22:48:03 +0300

    коммит Zadacha_19.csproj

--- Zadacha_19/Program.cs
+++ Zadacha_19/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// пятизначное число и проверяет, является ли 
+// оно палиндромом.
+Console.Clear();
+int DataNumber(string str)
+{
+   Console.Write(str);
+   int number = int.Parse(Console.ReadLine());
+   return number;
+}
+void Palindrom(int x)
+{
+   char[] array = x.ToString().ToCharArray();
+   int size = array.Length;
+   int i = 1;
+   int numb = x;
+   int numbA = 0;
+   int numbB = 0;
+
+   while(i <= size)
+   {
+      numbA = numb % 10;
+      numbB = numbA + numbB;
+      numbB = numbB * 10;
+      numb = numb / 10;
+      i++;
+   }
+   numbB = numbB / 10;
+   Console.WriteLine(numbB);
+   //Console.WriteLine(numb);
+
+   if(x == numbB)
+   {
+     Console.WriteLine(x + " -> да"); 
+   }
+  else
+  {
+     Console.WriteLine(x + " -> нет");
+  } 
+}
+
+int x = DataNumber("введите пятизначное число ");
+
+Palindrom(x);
