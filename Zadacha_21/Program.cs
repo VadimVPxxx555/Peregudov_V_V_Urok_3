2fa2458f74d7edbfd93a4d446e254375147674fb
Vadim V P <Everestxxx555@yandex.ru>
2022-12-16 23:45:12 +0300

    коммит Zadacha_21

--- Zadacha_21/Program.cs
+++ Zadacha_21/Program.cs
@@ -10,17 +10,19 @@ int DataEntryXYZ(string str)
   return number;
 }
 
-double CalcDistance(int x1, int y1, int x2, int y2)
+double CalcDistance(int x1, int y1, int z1, int x2, int y2, int z2)
 {
-  double result = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2));
+  double result = Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2));
   return result;
 }
 
 int x1 = DataEntryXYZ("Введите координаты x1: ");
 int y1 = DataEntryXYZ("Введите координаты y1: ");
+int z1 = DataEntryXYZ("Введите координаты z1: ");
 
 int x2 = DataEntryXYZ("Введите координаты x2: ");
 int y2 = DataEntryXYZ("Введите координаты y2: ");
+int z2 = DataEntryXYZ("Введите координаты z2: ");
 
-double result = CalcDistance(x1, y1, x2, y2);
+double result = CalcDistance(x1, y1, z1, x2, y2, z2);
 Console.WriteLine("Расстояние между точками: " + result);
